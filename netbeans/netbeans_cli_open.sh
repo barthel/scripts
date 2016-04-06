16ede42c7dd1c3548c88c64cb133072f23b313bc
barthel <barthel@users.noreply.github.com>
2016-04-06 15:44:38 +0200

    Add script to open a list of files in netbeans.

---
+++ netbeans/netbeans_cli_open.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# activate job monitoring
+# @see: http://www.linuxforums.org/forum/programming-scripting/139939-fg-no-job-control-script.html
+set -m
+# set -x
+
+if [ -z "${netbeans_exec}" ]
+  then
+    netbeans_exec="${NETBEANS_HOME}/bin/netbeans --close-group --jdkhome ${JAVA_HOME}/ -J-Xms40m -J-Xmx2048m -J-Dorg.eclipse.swt.browser.DefaultType=mozilla -J-Djava.library.path=/usr/lib/jni:/usr/lib/x86_64-linux-gnu/jni "
+fi
+netbeans_cli_open_exec="${netbeans_exec} --nosplash --open "
+
+
+${netbeans_cli_open_exec} $@ &
+
