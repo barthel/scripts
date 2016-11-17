8720d3d6c068291fedd81846d7187fe15684153e
barthel <barthel@users.noreply.github.com>
2016-11-17 09:43:26 +0100

    Adapts new screen names.

--- screenlayout/multi_screen.sh
+++ screenlayout/multi_screen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #xrandr --output VBOX0 --mode 1920x1200 --pos 0x0 --rotate normal --output VBOX1 --mode 1920x1200 --pos 1920x0 --rotate normal
-xrandr --output VGA-0 --mode 1920x1200 --pos 0x0 --rotate normal --output VGA-1 --mode 1920x1200 --pos 1920x0 --rotate normal
+#xrandr --output VGA-0 --mode 1920x1200 --pos 0x0 --rotate normal --output VGA-1 --mode 1920x1200 --pos 1920x0 --rotate normal
+xrandr --output VGA-1 --mode 1920x1200 --pos 0x0 --rotate normal --output VGA-2 --mode 1920x1200 --pos 1920x0 --rotate normal
 # reset desktop switch panel plugin
 dbus-send --print-reply --dest=org.xfce.Panel /org/xfce/Panel org.xfce.Panel.Terminate boolean:true
 
