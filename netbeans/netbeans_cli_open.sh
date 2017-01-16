9d21b12986fc3953184edfa6026014fad7e5d873
barthel <barthel@users.noreply.github.com>
2017-01-16 07:46:37 +0100

    Activate splash screen on netbeans CLI startup.

--- netbeans/netbeans_cli_open.sh
+++ netbeans/netbeans_cli_open.sh
@@ -9,7 +9,7 @@ if [ -z "${netbeans_exec}" ]
   then
     netbeans_exec="${NETBEANS_HOME}/bin/netbeans --jdkhome ${JAVA_HOME}/ -J-Xms40m -J-Xmx2048m -J-Dorg.eclipse.swt.browser.DefaultType=mozilla -J-Djava.library.path=/usr/lib/jni:/usr/lib/x86_64-linux-gnu/jni "
 fi
-netbeans_cli_open_exec="${netbeans_exec} --close-group --nosplash --open "
+netbeans_cli_open_exec="${netbeans_exec} --close-group --open "
 
 
 ${netbeans_cli_open_exec} $@ &
